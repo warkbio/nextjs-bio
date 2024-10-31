2dd26b029f1f431ecd910770a98590d3242ea929
Thegrab <thegrabm@gmail.com>
2024-10-31 15:53:53 +0100

    Actualizar global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Wark-BIO';
+    : 'DaríoFD';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Darío57923_';
+    : 'Mis proyectos.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Wark LLC. © Todos los derechos reservados.';
